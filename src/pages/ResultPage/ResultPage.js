cc8584073cf5a7aee87de23d883f051e876e2b20
luciD <sosalnet@yandex.ru>
2023-09-21 17:15:10 +0300

    fixed result-page

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -2,34 +2,36 @@ import { LeftFilterBar } from "it-events-frontend";
 import { CardList } from "it-events-frontend";
 import { useEventsContext } from "it-events-frontend";
 import { TopFilterBar } from "it-events-frontend";
-import { apiEvents } from "it-events-frontend";
+import { PageTitle } from "it-events-frontend";
 
 export default function ResultPage() {
-    const { popularEvents } = useEventsContext();
-
-    const { searchRequest, setSearchRequest  } = useEventsContext();
-
-    const fetchSelectedEvent = async (id) => {
-        try {
-          const data = await apiEvents.getSelectedEvent(id);
-          setSearchRequest(data)
-        } catch (error) {
-             console.error("Ошибка при выполнении запроса:", error);
-        }
-      };
+    const { searchResult, popularEvents } = useEventsContext();
 
     return (
         <section className='search__container'>
             <LeftFilterBar />
             <div className='search__box'>
                 <TopFilterBar />
-                <CardList
-                    title=""
-                    listDirection="col"
-                    cardDirection="row"
-                    style={{ gap: "22px" }}
-                    events={searchRequest}
-                />
+                {searchResult.length ? (
+                    <CardList
+                        title=""
+                        listDirection="col"
+                        cardDirection="row"
+                        style={{ gap: "22px" }}
+                        events={searchResult}
+                    />
+                ) : (
+                    <>
+                        <PageTitle title="Ничего не нашлось" subtitle="Но нам есть, что предложить" size={50} />
+                        <CardList
+                            title="Популяроное"
+                            listDirection="col"
+                            cardDirection="row"
+                            style={{ gap: "22px" }}
+                            events={popularEvents}
+                        />
+                    </>
+                )}
             </div>
         </section>
     )
